318e76cf93a08ba70132571e6b818edf785051dc
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-05-25 17:01:32 +0500

    end todo list

--- js/index.js
+++ js/index.js
@@ -15,11 +15,17 @@ function createTodoItem(todo) {
 
   elLi.className = "d-flex align-items-center py-2 px-3 border-bottom";
 
+  elCheckboxInput.checked = todo.isCompleted
   elCheckboxInput.type = "checkbox";
-  elCheckboxInput.className = "form-check-input mt-0";
+  elCheckboxInput.className = "form-check-input mt-0 completed";
+  elCheckboxInput.dataset.id = todo.id;
 
   elText.textContent = todo.title;
   elText.className = "m-0 ms-2";
+  if (todo.isCompleted) {
+    elText.classList.add('text-decoration-line-through')
+    elText.classList.add('text-muted')
+  }
 
   elDiv.className = "ms-auto";
 
@@ -81,14 +87,23 @@ let handelEditTodo = (evt) => {
   let foundTodoIndex = todos.findIndex((element) => element.id === evt.target.dataset.id)
   let editText = prompt('O\'zgartir', todos[foundTodoIndex].title)
   todos[foundTodoIndex].title = editText
-  
+
   renderElements(todos)
+  localStorage.setItem('todos', JSON.stringify(todos))
 }
 
+let handelCompletedTodo = (evt) => {
+  let foundTodoIndex = todos.findIndex((element) => element.id === evt.target.dataset.id)
+  todos[foundTodoIndex].isCompleted = evt.target.checked
+
+  renderElements(todos)
+  localStorage.setItem('todos', JSON.stringify(todos))
+} 
 
 elInput.addEventListener('keyup', handelAddTodo);
 elList.addEventListener('click', (evt) => {
   if (evt.target.matches('.delete')) return handelDeleteTodo(evt)
   if (evt.target.matches('.edit')) return handelEditTodo(evt)
+  if (evt.target.matches('.completed')) return handelCompletedTodo(evt)
 })
 renderElements(todos);
